446121e28f1255a1c07f2ac1d9b7c30f559bdb8c
halisa <halisasaipulla@gmail.com>
2023-03-24 18:07:17 -0700

    deploy process

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 // API Key-https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = process.env.API_KEY;
+export const API_KEY = process.env.API_KEY;;
+
 // context key - https://cse.google.com/cse/creat
 export const CONTEXT_KEY = process.env.CONTEXT_KEY;
